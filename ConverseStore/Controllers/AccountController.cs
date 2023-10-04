e825de6c9becb6bd463106809ee4354f54ade1ad
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-04 08:36:45 +0330

    Ready To Add Account Management

---
+++ ConverseStore/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Context.Data;
+using Context.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConverseStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly StoreDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager,
+                                    SignInManager<AppUser> signInManager,
+                                    StoreDbContext applicationDBContext)
+        {
+            _context = applicationDBContext;
+            _signInManager = signInManager;
+            _userManager = userManager;
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
